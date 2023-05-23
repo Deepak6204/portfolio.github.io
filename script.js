4d830ef1d17e7253761212697a36a0c4e9221731
Deepak6204 <127977680+Deepak6204@users.noreply.github.com>
2023-05-23 17:40:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 window.addEventListener("scroll", checkViewport);
 
+window.addEventListener("scroll",addfadeout);
+function addfadeout(){
+  var element = document.getElementsByClassName('container2')[0];
+
+  var area = element.getBoundingClientRect();
+  if(area.top < window.innerHeight+10){
+    setTimeout(element.classList.add("animate"), 2000);
+  }
+}
+
+
 function checkViewport() {
   var element = document.getElementById("container");
   var element2 = document.getElementsByTagName("nav")[0];
@@ -12,14 +23,17 @@ function checkViewport() {
 
   if (rect.top < 0) {
     element2.style.backgroundColor = "white";
+
   }
   if (rect.top >= 0) {
     element2.style.backgroundColor = "#0e131f00";
+
   }
 
   let a = ["html", "css", "js", "c", "cpp", "python"];
   var langlist = document.getElementsByClassName("ele");
   if (skillrect.top < window.innerHeight) {
+    skills.classList.add("skillbox-animation");
     for (let i = 0; i < 6; i++) {
       if (langlist[i].getBoundingClientRect().bottom < window.innerHeight) {
         setTimeout(progressbar[i].classList.add(a[i]), 1000);
@@ -38,7 +52,7 @@ function type() {
   if (index <= text.length) {
     typewriter.textContent = text.slice(0, index); // Updates the text content
     index++;
-    setTimeout(type, 100); // Controls the typing speed (adjust as needed)
+    setTimeout(type, 200); // Controls the typing speed (adjust as needed)
   }
   if(index > text.length){
     tc.style = "border: none";
